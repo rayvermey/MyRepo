a7d5b71071a7a6e60ec106d62501082b6029033b
Ray Vermey <rayvermey@gmail.com>
2020-01-08 15:06:16 +0100

    New Update

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -7,56 +7,33 @@ function run {
   fi
 }
 
-#run "xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal"
-#run "xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
+run "xrandr --output VGA-1 --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output VIRTUAL1 --off"
 run "nm-applet"
-#run "caffeine"
-run "pamac-tray"
 run "variety"
-#run "xfce4-power-manager"
 run "blueberry-tray"
 run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
 run "numlockx on"
 run "volumeicon"
-#run "nitrogen --restore"
-run "conky -c $HOME/.config/awesome/system-overview"
+#run "conky -c $HOME/.config/awesome/system-overview"
 
-run "dropbox"
+run "termite -e gotop"
+run "compton"
 run "spotify"
-run "caprine"
-run "stack-client"
 run "autokey-gtk"
-run "xfce4-clipman"
-run "slack"
-run "discord"
-run "telegram-desktop"
+run "copyq"
+#run "xfce4-clipman"
 run "dunst"
-run "sxhkd"
+run "mpd"
+run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc 
+run "vivaldi-stable"
+run "/home/ray/scripts/tmp_bspwm_adaptive_marked_border"
+run "pcloud &"
+run "station"
+
 xsetroot -cursor_name left_ptr &                                                                                                
-if ! pgrep -f ICE-SSB-facebook ;
-	then	
-	firefox -new-instance --class ICE-SSB-facebook --profile /home/ray/.local/share/ice/firefox/facebook  http://www.facebook.com &
-fi
-
-if ! pgrep -f ICE-SSB-whatsap ;
-then
-       	firefox -new-instance --class ICE-SSB-whatsapp --profile /home/ray/.local/share/ice/firefox/whatsapp  https://web.whatsapp.com &
-fi
-
-if ! pgrep -f ICE-SSB-evernote ;
-then firefox -new-instance --class ICE-SSB-evernote --profile /home/ray/.local/share/ice/firefox/evernote  https://www.evernote.com/Login.action &
-fi
-
-if ! pgrep -f ICE-SSB-quora ;
-then  firefox -new-instance --class ICE-SSB-quora --profile /home/ray/.local/share/ice/firefox/quora --no-remote http://nl.quora.com &
-fi
-
-if ! pgrep -f ICE-SSB-trello ;
-then firefox --class ICE-SSB-trello --profile /home/ray/.local/share/ice/firefox/trello --no-remote https://trello.com/b/geEKJdMo/backlog-paravisie &
-fi
-
-if ! pgrep -f ICE-SSB-todoist ;
-then firefox --class ICE-SSB-todoist --profile /home/ray/.local/share/ice/firefox/todoist --no-remote https://todoist.com/app &
-fi
-
-run "vivaldi-snapshot"
+sleep 1
+wmctrl -r Spotify -t 8
+#if ! pgrep -f ICE-SSB-facebook ;
+#	then	
+#	firefox -new-instance --class ICE-SSB-facebook --profile /home/ray/.local/share/ice/firefox/facebook  http://www.facebook.com &
+#fi
