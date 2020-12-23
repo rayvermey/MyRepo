ee898692185d36daf4c56441edb091cb3e0994a8
Ray Vermey <rayvermey@gmail.com>
2020-12-23 17:10:14 +0100

    DELETED STUFF

--- DESKTOP-RAY/bspwm/scripts/show_help.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' ~/.config/sxhkd/sxhkdrc | column -t -s $'\t'  | rofi -dmenu -i -no-show-icons -width 1000
-
-#cat  ~/.config/sxhkd/sxhkdrc | awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' | column -t -s $'\t' | rofi -dmenu -i -no-show-icons -width 1000
